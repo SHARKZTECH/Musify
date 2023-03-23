1e80fda92c4ea610370af26523c6042c60b580dc
sharkz <mosenjemudogoz@gmail.com>
2023-03-23 15:46:28 +0300

    home activity fab extend

--- app/src/main/java/com/example/musify/HomeActivity.java
+++ app/src/main/java/com/example/musify/HomeActivity.java
@@ -3,6 +3,7 @@ package com.example.musify;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -25,23 +26,30 @@ public class HomeActivity extends AppCompatActivity {
         btnLiked=binding.btnLiked;
         btnRecent=binding.btnRecent;
 
+        fabExtend();
+
+    }
+    private void fabExtend(){
+
         btnAlbum.setVisibility(View.GONE);
         btnArtist.setVisibility(View.GONE);
         btnLiked.setVisibility(View.GONE);
         btnRecent.setVisibility(View.GONE);
 
         isAllFabVisible=false;
-
         binding.btnFab.shrink();
 
         binding.btnFab.setOnClickListener(view->{
             if(!isAllFabVisible){
-                 btnAlbum.setVisibility(View.VISIBLE);
-                 btnArtist.setVisibility(View.VISIBLE);
-                 btnLiked.setVisibility(View.VISIBLE);
-                 btnRecent.setVisibility(View.VISIBLE);
+                btnAlbum.setVisibility(View.VISIBLE);
+                btnArtist.setVisibility(View.VISIBLE);
+                btnLiked.setVisibility(View.VISIBLE);
+                btnRecent.setVisibility(View.VISIBLE);
 
                 binding.btnFab.extend();
+                binding.btnFab.setIcon(getDrawable(R.drawable.unfold));
+                binding.btnFab.setIconTint(getResources().getColorStateList(R.color.white));
+                binding.btnFab.setBackgroundTintList(getColorStateList(R.color.fab_bg2));
                 isAllFabVisible=true;
             }else{
                 btnAlbum.setVisibility(View.GONE);
@@ -50,6 +58,10 @@ public class HomeActivity extends AppCompatActivity {
                 btnRecent.setVisibility(View.GONE);
 
                 binding.btnFab.shrink();
+                binding.btnFab.setIcon(getDrawable(R.drawable.sp_logo));
+                binding.btnFab.setIconTint(getResources().getColorStateList(R.color.fab_bg1));
+                binding.btnFab.setBackgroundTintList(getColorStateList(R.color.bg_1));
+
                 isAllFabVisible=false;
             }
         });
