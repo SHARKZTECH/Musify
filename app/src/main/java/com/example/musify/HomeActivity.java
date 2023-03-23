52e1c31c7aa60ee4da34ea9883feddcbeddc97bb
sharkz <mosenjemudogoz@gmail.com>
2023-03-23 14:48:58 +0300

    home activity binding and fab extend

--- app/src/main/java/com/example/musify/HomeActivity.java
+++ app/src/main/java/com/example/musify/HomeActivity.java
@@ -5,16 +5,53 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import com.example.musify.databinding.ActivityHomeBinding;
 
 public class HomeActivity extends AppCompatActivity {
     ActivityHomeBinding binding;
+    Boolean isAllFabVisible;
+    LinearLayout btnAlbum,btnArtist,btnLiked,btnRecent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding=ActivityHomeBinding.inflate(getLayoutInflater());
-        View view=binding.getRoot();
-        setContentView(view);
+        View rootView=binding.getRoot();
+        setContentView(rootView);
+
+        btnAlbum=binding.btnAlbum;
+        btnArtist=binding.btnArtist;
+        btnLiked=binding.btnLiked;
+        btnRecent=binding.btnRecent;
+
+        btnAlbum.setVisibility(View.GONE);
+        btnArtist.setVisibility(View.GONE);
+        btnLiked.setVisibility(View.GONE);
+        btnRecent.setVisibility(View.GONE);
+
+        isAllFabVisible=false;
+
+        binding.btnFab.shrink();
+
+        binding.btnFab.setOnClickListener(view->{
+            if(!isAllFabVisible){
+                 btnAlbum.setVisibility(View.VISIBLE);
+                 btnArtist.setVisibility(View.VISIBLE);
+                 btnLiked.setVisibility(View.VISIBLE);
+                 btnRecent.setVisibility(View.VISIBLE);
+
+                binding.btnFab.extend();
+                isAllFabVisible=true;
+            }else{
+                btnAlbum.setVisibility(View.GONE);
+                btnArtist.setVisibility(View.GONE);
+                btnLiked.setVisibility(View.GONE);
+                btnRecent.setVisibility(View.GONE);
+
+                binding.btnFab.shrink();
+                isAllFabVisible=false;
+            }
+        });
     }
 }
