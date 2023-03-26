6a9b21a23a9cd0fc7512cacd3e700fde5e3682c5
sharkz <mosenjemudogoz@gmail.com>
2023-03-26 14:37:09 +0300

    playlistcontent

---
+++ app/src/main/java/com/example/musify/PlayListContentActivity.java
@@ -0,0 +1,14 @@
+package com.example.musify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlayListContentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_list_content);
+    }
+}
